8e5a37f7e61f51aa09118190b1f9fc27610e5119
Hazar Gul Nazari <nazariazargul@gmail.com>
2019-08-16 10:30:52 +0200

    newChanges

--- src/main/java/Java9/Lesson1.java
+++ src/main/java/Java9/Lesson1.java
@@ -2,17 +2,18 @@ package Java9;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-interface Util{
-	void doit();
-	default void doSomethingElseToo() {
-		privateOperation();
-		System.out.println("Here is another thing to do ");
-	}
-	private void privateOperation() {
-		System.out.println("Operation Dependencies");
-	}
-}
+//interface Util{
+//	void doit();
+//	default void doSomethingElseToo() {
+//		privateOperation();
+//		System.out.println("Here is another thing to do ");
+//	}
+//	private void privateOperation() {
+//		System.out.println("Operation Dependencies");
+//	}
+//}
 
 public class Lesson1 {
 	
@@ -29,7 +30,23 @@ public class Lesson1 {
 		// break the look while the condition is met
 //		numbers.stream().takeWhile(num -> num > 10).forEach(System.out::println);
 		
-		numbers.stream().dropWhile(num -> num % 2 == 0).forEach(System.out::println);
+		//numbers.stream().dropWhile(num -> num % 2 == 0).forEach(System.out::println);
+		
+		
+		 // Antoher way of crewating list and set
+//		 Set<Integer> set = Set.of(1,2,3,4,5);
+		
+//		List<Integer> list = List.of(1,2,3,4,5);
+		
+//		IntStream.iterate(0, i -> i < 5, i -> i + 1).forEach(System.out::println);
+		
+//		IntStream.iterate(i, i -> i + 2).limit(3).forEach(System.out::println);
+		
+//		Optional<Integer> first numbers.stream().filter(e -> e < 5).findFirst();
+		
+//		first.ifPresentOrElse(e -> System.out.rintln(e), ()-> System.out.println("Doesn't exist"));
+		
+		
 		
 	}
 
